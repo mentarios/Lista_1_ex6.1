38eee9feafd69c3b47565b283f465f02de615111
mentarios <matmentarios@gmail.com>
2024-02-27 14:51:35 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex6.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //usando mod
+            double n;
+            Console.WriteLine("digite um numero inteiro");
+            n = double.Parse(Console.ReadLine());
+            if ((int)n % 4 == 0)
+            {
+                Console.WriteLine("numero valido");
+            }
+            else
+            {
+                Console.WriteLine("numero invalido");
+            }
+        }
+    }
+}
